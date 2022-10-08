56b35fbc1511930e1bfcbb1b3f7bd0f0f2137542
Gaurav Ramdas Ingole <114977055+g-ingole@users.noreply.github.com>
2022-10-08 17:36:25 +0530

    Add files via upload

---
+++ ascii.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char chr;
+    cout<<"enter the character";
+    cin>>chr;
+    cout<<"entered character ="<<chr;
+    cout<<"\n ASCII value is "<<int(chr);
+}
